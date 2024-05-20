32d35c8ab0368144ee13b9a499532b6580634163
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-05-20 12:24:41 -0300

    v1.04
    - Criando componente "Integrantes"

---
+++ src/app/components/Integrantes/components/Card/index.tsx
@@ -0,0 +1,15 @@
+type CardProps = {
+    title: string;
+    names: string[]
+}
+
+export default function Card({title, names}: CardProps) {
+ return (
+   <div className="w-72 md:w-full flex flex-col justify-start items-start my-2">
+    <h1 className="bg-blue-600 text-white px-5 py-2 rounded rounded-bl-none">{title}</h1>
+    <div className="text-blue-600 bg-white flex flex-col justify-center items-center w-full gap-y-2 py-4 rounded rounded-tl-none">
+        {names.map(name => <p className="w-full text-center">{name}</p>)}
+    </div>
+   </div>
+ );
+}
