d1017b94d693d00eeef3d3a691521c8eb5b66236
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-05-20 12:36:38 -0300

    v1.08
    - Adicionando key

--- src/app/components/Integrantes/components/Card/index.tsx
+++ src/app/components/Integrantes/components/Card/index.tsx
@@ -8,7 +8,7 @@ export default function Card({title, names}: CardProps) {
    <div className="w-72 md:w-full flex flex-col justify-start items-start my-2">
     <h1 className="bg-blue-600 text-white px-5 py-2 rounded rounded-bl-none">{title}</h1>
     <div className="text-blue-600 bg-white flex flex-col justify-center items-center w-full gap-y-2 py-4 rounded rounded-tl-none">
-        {names.map(name => <p className="w-full text-center">{name}</p>)}
+        {names.map(name => <p key={name} className="w-full text-center">{name}</p>)}
     </div>
    </div>
  );
