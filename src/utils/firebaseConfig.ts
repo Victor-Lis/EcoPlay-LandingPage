59b9e2149a8efde84168aae404b03183b0d8e0a0
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-09-25 00:32:08 -0300

    Omitindo keys particulares

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,17 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref } from "firebase/database";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyD23E6djYvzjNPu3Oqgn_6OttrusrgabzM",
-//     authDomain: "projeto-feteps.firebaseapp.com",
-//     databaseURL: "https://projeto-feteps-default-rtdb.firebaseio.com",
-//     projectId: "projeto-feteps",
-//     storageBucket: "projeto-feteps.appspot.com",
-//     messagingSenderId: "754643098343",
-//     appId: "1:754643098343:web:0d76d9a532ddc6f4c42042",
-//     measurementId: "G-CPSQWN63SR"
-//   };
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
