ccbffab5ed6c0f54f5ef5be69802575b580650b9
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-10-16 23:59:55 -0300

    feat: removendo senha da api

--- src/app/api/tampinha/route.ts
+++ src/app/api/tampinha/route.ts
@@ -7,22 +7,18 @@ const isSmallerThenTen = (n: number) => (n < 10 ? `0${n}` : n);
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const senha = searchParams.get("senha");
+  // const senha = searchParams.get("senha");
   try {
-    if (senha == process.env.PASSWORD) {
+    // if (senha == process.env.PASSWORD) {
       const today = new Date();
       await push(tampinhasRef, {
-        data: `${isSmallerThenTen(today.getDate())}/${isSmallerThenTen(
-          today.getMonth() + 1
-        )}/${today.getFullYear()}`,
-        hora: `${isSmallerThenTen(today.getHours())}:${isSmallerThenTen(
-          today.getMinutes()
-        )}:${isSmallerThenTen(today.getSeconds())}`,
+        data: `${isSmallerThenTen(today.getDate())}/${isSmallerThenTen(today.getMonth() + 1)}/${today.getFullYear()}`,
+        hora: `${isSmallerThenTen(today.getHours())}:${isSmallerThenTen(today.getMinutes())}:${isSmallerThenTen(today.getSeconds())}`,
       });
       return NextResponse.json({ status: 200 });
-    }else {
-      throw new Error("Senha incorreta")
-    }
+    // }else {
+      // throw new Error("Senha incorreta")
+    // }
   } catch (error: any) {
     return NextResponse.json({ error: error?.message ? error.message : "Tampinha não salva!" }, { status: 400 });
   }
